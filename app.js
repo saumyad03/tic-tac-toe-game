56df024e67848f06870d52f052ec810dc928b3a9
saumyad03 <saumyad03@gmail.com>
2022-06-06 08:34:51 -0400

    fixed bugs pertaining to winning/losing/draws

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+//Game status
+let gameOver = false;
 //Sets color for player and AI
 let playerColor = "blue";
 let aiColor = "red";
-//Sets default AI difficulty and gets buttons for setting difficulty
+//Sets default AI difficulty and gets buttons for setting difficulty and resetting
 let aiDifficulty = "Hard"
 const easyBtn = document.querySelector("#easy-btn");
 const hardBtn = document.querySelector("#hard-btn");
+const resetBtn = document.querySelector("#reset-btn");
 //Gets array of all the boxes
 const boxes = document.querySelectorAll(".box");
 console.log(boxes);
@@ -12,26 +15,39 @@ console.log(boxes);
 for (const box of boxes) {
     box.addEventListener("click", (e) => {
         //Places player's move and generates AI move if box isn't already occupied
-        if (e.target.textContent == "") {
+        if (gameOver == true) { console.log("gameover"); }
+        if (e.target.textContent == "" && gameOver == false) {
             e.target.textContent = "X";
             e.target.style.color = playerColor;
-            checkDraw();
-            checkWin("X");
+            if (checkDraw()) {
+                gameOver = true;
+                return;
+            };
+            if (checkWin("X", true)) {
+                gameOver = true;
+                return;
+            };
             aiMove();
-            checkWin("O");
+            if (checkWin("O", true)) {
+                gameOver = true;
+                return;
+            };
         }
     });
 }
 //Changes AI difficulty to easy if easy button is clicked
 easyBtn.addEventListener("click", (e) => {
     aiDifficulty = "Easy";
-    alert(aiDifficulty);
+    reset();
+    console.log(aiDifficulty);
 })
 //Changes AI difficulty to hard if hard button is clicked
 hardBtn.addEventListener("click", (e) => {
     aiDifficulty = "Hard";
-    alert(aiDifficulty);
+    reset();
+    console.log(aiDifficulty);
 })
+resetBtn.addEventListener("click", reset);
 //Generates AI move based on difficulty
 function aiMove() {
     let aiBox;
@@ -81,30 +97,52 @@ function checkDraw() {
             return false;
         }
     }
-    alert("draw");
     return true;
 }
 //Checks if certain symbol won
-function checkWin(symbol) {
+function checkWin(symbol, end = false) {
     //Check left-right win
     for (let i = 0; i < boxes.length; i+= 3) {
         if (boxes[i].textContent == symbol && boxes[i+1].textContent == symbol && boxes[i+2].textContent ==symbol) {
+            if (end == true) {
+                gameOver = true;
+                console.log(symbol + "won");
+            }
             return true;
         }
     }
     //Checks up down win
     for (let i = 0; i < 3; i++) {
         if (boxes[i].textContent == symbol && boxes[i+3].textContent == symbol && boxes[i+6].textContent == symbol) {
+            if (end == true) {
+                gameOver = true;
+                console.log(symbol + "won");
+            }
             return true;
         }
     }
     //Checks win top-left to bottom-right
     if (boxes[0].textContent == symbol && boxes[4].textContent == symbol && boxes[8].textContent == symbol) {
+        if (end == true) {
+            gameOver = true;
+            console.log(symbol + "won");
+        }
         return true;
     }
     //Checks win top-right to bottom-left
     if (boxes[2].textContent == symbol && boxes[4].textContent == symbol && boxes[6].textContent == symbol) {
+        if (end = true) {
+            gameOver == true;
+            console.log(symbol + "won");
+        }
         return true;
     }
     return false;
+}
+function reset() {
+    for (const box of boxes) {
+        box.textContent = "";
+        box.style.color = "black";
+        gameOver = false;
+    }
 }
